f0cb3256ad0ed0684f74b663c6c739a56d61e18a
Sam <parkssa123@gmail.com>
2023-12-18 08:55:29 -0500

    updates

--- packages/skyflow_android/android/src/main/kotlin/com/flutter/skyflow/SkyflowAndroidPlugin.kt
+++ packages/skyflow_android/android/src/main/kotlin/com/flutter/skyflow/SkyflowAndroidPlugin.kt
@@ -13,6 +13,7 @@ import io.flutter.plugin.common.MethodChannel.Result
 import okhttp3.Headers.Companion.toHeaders
 import org.json.JSONObject
 import okhttp3.OkHttpClient
+import org.json.JSONArray
 import java.io.IOException
 
 
@@ -44,42 +45,87 @@ ${initializationError ?: "Skyflow SDK did not initialize."}""",
         }
         when (call.method) {
             "initialize" -> {
-                val arguments = call.arguments as Map<String,Any>
+                val arguments = call.arguments as JSONObject
+
                 val tokenProviderURL = arguments["tokenProviderURL"] as String
-                val headers = arguments["headers"] as Map<String, String>
+                val headers = arguments["headers"] as JSONObject
                 val vaultID = arguments["vaultID"] as String
                 val vaultURL = arguments["vaultURL"]  as String
                 val env = arguments["env"] as String
+                val headerMap = headers.toMap() as Map<String,String>;
 
                 return initializeSkyflowClient(
-                    tokenProviderURL,
-                    headers,
+                    tokenProviderURL, headerMap,
                     vaultID,
                     vaultURL,
                     env,
                     result
                 )
             }
+            "insert" -> {
+                val records = call.argument<JSONObject>("records")!!
+                val options = call.argument<JSONObject>("options")
+                return insert(records, options, result)
+                
+            }
+            "detokenize" -> {
+                val records = call.argument<JSONObject>("records")!!
+                return detokenize(records, result)
+            }
+            
             else -> result.notImplemented()
         }
     }
 
-    override fun onAttachedToActivity(binding: ActivityPluginBinding){
-
-    }
+    override fun onAttachedToActivity(binding: ActivityPluginBinding){}
 
 
     override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
     }
 
-    override fun onDetachedFromActivityForConfigChanges() {
+    override fun onDetachedFromActivityForConfigChanges() {}
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
+
+    override fun onDetachedFromActivity() {}
+
+    fun insert(
+        records: JSONObject,
+        options: JSONObject?,
+        @NonNull result: Result
+    ) {
+        try {
+            var insertOptions = InsertOptions()
+
+            if(options != null && options.has("tokens") && options.has("upsert")){
+                insertOptions = InsertOptions(
+                        options["tokens"] as Boolean,
+                        options["upsert"] as JSONArray?
+                )
+            }
+
+            val flutterCallback = FlutterCallback(result)
+            
+            skyflowClient!!.insert(records, insertOptions,flutterCallback)
+            
+        } catch (e: Exception) {
+            result.error("insert failed", e.message, null)
+        }
     }
 
-    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    }
+    fun detokenize(
+        records: JSONObject,
+        @NonNull result: Result
+    ) {
+        try {
+            val flutterCallback = FlutterCallback(result)
 
-    override fun onDetachedFromActivity() {
+            skyflowClient!!.detokenize(records, flutterCallback)
+            
+        } catch (e: Exception) {
+            result.error("detokenize failed", e.message, null)
+        }
     }
 
     fun initializeSkyflowClient(
@@ -115,8 +161,21 @@ ${initializationError ?: "Skyflow SDK did not initialize."}""",
         }
     }
 
-
+    fun JSONObject.toMap(): Map<String, *> = keys().asSequence().associateWith {
+        when (val value = this[it])
+        {
+            is JSONArray ->
+            {
+                val map = (0 until value.length()).associate { Pair(it.toString(), value[it]) }
+                JSONObject(map).toMap().values.toList()
+            }
+            is JSONObject -> value.toMap()
+            JSONObject.NULL -> null
+            else            -> value
+        }
+    }
 }
+
 
 class FlutterTokenProvider: Skyflow.TokenProvider {
     private var tokenEndpoint: String
@@ -128,6 +187,7 @@ class FlutterTokenProvider: Skyflow.TokenProvider {
     }
 
     override fun getBearerToken(callback: Callback) {
+        println(headers)
         val request = okhttp3.Request.Builder()
             .url(tokenEndpoint)
             .headers(headers.toHeaders())
@@ -144,7 +204,13 @@ class FlutterTokenProvider: Skyflow.TokenProvider {
 
                         val accessTokenObject = JSONObject(responseBody!!.string().toString())
 
-                        val tokenData = accessTokenObject["data"] as Map<String, String>
+                        println(accessTokenObject)
+
+                        if(!accessTokenObject.has("data")){
+                            callback.onFailure("The response from the server did not have the expection values")
+                        }
+
+                        val tokenData = accessTokenObject["data"] as JSONObject
                         val accessToken = tokenData["accessToken"]
                         callback.onSuccess("$accessToken")
                     }
@@ -154,5 +220,21 @@ class FlutterTokenProvider: Skyflow.TokenProvider {
         }catch (exception:Exception){
             callback.onFailure(exception)
         }
+    }
+}
+
+class FlutterCallback: Skyflow.Callback {
+    var resultCallback: Result;
+    
+    constructor(resultCallback: Result) {
+        this.resultCallback = resultCallback
+    }
+
+    override fun onSuccess(responseBody: Any) {
+        resultCallback.success(responseBody)
+    }
+
+    override fun onFailure(exception: Any) {
+        resultCallback.error("insert failed", exception.toString(), null)
     }
 }
